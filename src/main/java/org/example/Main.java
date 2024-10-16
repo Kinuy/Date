3669efb4ba196092c212592fc3cb9847ac707e93
Tobias Weber <toby-weber@gmx.de>
2024-10-16 10:58:56 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        /*
+        * Step 1: Write code to output the current date and time.
+
+        Step 2: Add a timespan of 2 weeks to the current date and output the new date.
+
+        Step 3: Compare the current date with a specified future date and output whether the current date is before or after the specified date.
+
+        Step 4: Calculate the difference in days between two arbitrary dates and output the result.
+* */
+
+
+    }
+}
